44c99c4c64d597b74aeda27b8fdf7165475e03f4
yuvaraj <yuvi.mageshkumar@gmail.com>
2020-01-02 18:05:23 +0530

    Ecomm

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE product_items(product_id int NOT NULL PRIMARY KEY, brand_name varchar(255),product_name varchar(255),qty int(255),price int(255));
+
+
+INSERT INTO product_items(product_id,brand_name,product_name,qty,price) VALUES (01,'apple','iphone7','1',24900);
